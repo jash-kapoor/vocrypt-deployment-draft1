ba4c878373e8e05dea8992ede3ffc6fc9d9a34b4
Jash Kapoor <jashmkapoor@gmail.com>
2025-10-08 01:27:01 +0530

    chore: move to render-backend-build.sh at root, simplify script

--- render-backend-build.sh
+++ render-backend-build.sh
@@ -3,15 +3,15 @@
 set -o errexit
 
 echo "Installing native dependencies..."
-# Install cmake and ffmpeg
 apt-get update && apt-get install -y cmake ffmpeg
 
 echo "Building ggwave binaries..."
-# Navigate to the ggwave directory from the repo root and build for Linux
-cd ../../ggwave
+# We're already at the root, so we just enter the ggwave dir
+cd ggwave
 cmake -S . -B build && cmake --build build -j
+cd .. # Go back to the root directory
 
 echo "Installing server dependencies..."
-# Navigate back to the server directory (our root directory)
-cd ../app/server
+# Now go into the server folder to run npm install
+cd app/server
 npm install
